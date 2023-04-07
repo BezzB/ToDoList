30652cca574e1ce13bc1a4e523da38c70fcf3013
BezzB <118984781+BezzB@users.noreply.github.com>
2023-04-07 11:03:11 +0300

    Update scripts.js

--- src/script.js
+++ src/script.js
@@ -1,38 +1,20 @@
 import './style.css';
 
+
 const addButton = document.querySelector('.addButton');
 const inputValue = document.querySelector('.input');
 const container = document.querySelector('.container');
-const tasks = [
-  {
-    description: 'Buy groceries',
-    completed: false,
-    index: 0,
-  },
-  {
-    description: 'Take out the trash',
-    completed: true,
-    index: 1,
-  },
-  {
-    description: 'Walk the dog',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'Do laundry',
-    completed: false,
-    index: 3,
-  },
-];
+const itemList = [];
 
 class Item {
-  constructor(itemName, index) {
-    this.createDiv(itemName, index);
+  constructor(itemName) {
+    this.itemName = itemName;
+    this.createDiv(itemName);
+    itemList.push(this.itemName);
+    this.saveItems();
   }
 
-  // eslint-disable-next-line no-unused-vars
-  createDiv(itemName, index) {
+  createDiv(itemName) {
     const input = document.createElement('input');
     input.value = itemName;
     input.disabled = true;
@@ -63,28 +45,34 @@ class Item {
     input.disabled = !input.disabled;
   }
 
-  // eslint-disable-next-line class-methods-use-this
   remove(itemBox) {
     container.removeChild(itemBox);
+    const index = itemList.indexOf(this.itemName);
+    if (index > -1) {
+      itemList.splice(index, 1);
+      this.saveItems();
+    }
+  }
+
+  // eslint-disable-next-line class-methods-use-this
+  saveItems() {
+    localStorage.setItem('itemList', JSON.stringify(itemList));
   }
 }
 
-// Iterate through the array of tasks and display them in the list
-tasks.forEach((task) => {
-  // eslint-disable-next-line no-new
-  new Item(task.description, task.index);
-});
+// Check if items are stored in local storage
+if (localStorage.getItem('itemList')) {
+  const storedItems = JSON.parse(localStorage.getItem('itemList'));
+  storedItems.forEach((item) => {
+    // eslint-disable-next-line no-new
+    new Item(item);
+  });
+}
 
 addButton.addEventListener('click', () => {
   if (inputValue.value !== '') {
-    const newTask = {
-      description: inputValue.value,
-      completed: false,
-      index: tasks.length + 1,
-    };
-    tasks.push(newTask);
     // eslint-disable-next-line no-new
-    new Item(newTask.description, newTask.index);
+    new Item(inputValue.value);
     inputValue.value = '';
   }
 });
