a50f74637b4f21ef23817c9f11692a9ac31436c4
BezzB <118984781+BezzB@users.noreply.github.com>
2023-04-15 12:35:13 +0300

    Update linters

--- src/AddTask.js
+++ src/AddTask.js
@@ -61,7 +61,7 @@ export const display = () => {
       });
 
       // eslint-disable-next-line no-unused-vars
-      saveBtn.addEventListener('click', (e) => {
+      saveBtn.addEventListener('click', (_e) => {
         saveBtn.style.display = 'none';
         editBtn.style.display = '';
         saveEdit(item);
@@ -71,7 +71,7 @@ export const display = () => {
       const deleteItem = document.createElement('button');
       deleteItem.className = 'delete-item';
       deleteItem.innerHTML = '<svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="30px" width="30px" xmlns="http://www.w3.org/2000/svg"><path d="M864 256H736v-80c0-35.3-28.7-64-64-64H352c-35.3 0-64 28.7-64 64v80H160c-17.7 0-32 14.3-32 32v32c0 4.4 3.6 8 8 8h60.4l24.7 523c1.6 34.1 29.8 61 63.9 61h454c34.2 0 62.3-26.8 63.9-61l24.7-523H888c4.4 0 8-3.6 8-8v-32c0-17.7-14.3-32-32-32zm-200 0H360v-72h304v72z"></path></svg>';
-      deleteItem.addEventListener('click', (e) => {
+      deleteItem.addEventListener('click', (_e) => {
         removeTask(indexNo);
         display();
       });
@@ -91,7 +91,7 @@ export const display = () => {
   }
 };
 
-window.addEventListener('DOMContentLoaded', (e) => {
+window.addEventListener('DOMContentLoaded', (_e) => {
   display();
   const form = document.getElementById('add-task-form');
 
